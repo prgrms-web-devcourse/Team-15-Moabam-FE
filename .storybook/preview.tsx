e0990a5a3be0ee2c6160b2889f32013093b68693
bbearcookie <aodem@naver.com>
2023-11-26 18:02:01 +0900

    chore: storybook 전역에 QueryClientProvider 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '../src/core/api/queryClient';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import '../src/main.css';
 
 const preview: Preview = {
   parameters: {
+    decorators: [
+      (Story) => (
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      )
+    ],
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
